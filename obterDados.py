e40db02f5623f7605c6bcae0c69e784e00a3529e
Aurelio Aquino <aurelio.obmep@gmail.com>
2023-07-09 18:20:09 -0400

    nome de var mais coerente

--- obterDados.py
+++ obterDados.py
@@ -1,11 +1,11 @@
 import MetaTrader5 as mt5
 import pandas as pd
 
-def obterSimbolo(simbolo, timeframe=mt5.TIMEFRAME_M5, diaAtual=0, n=99999):
+def obterSimbolo(simbolo, timeframe=mt5.TIMEFRAME_M5, delayCandles=0, n=99999):
     if not mt5.initialize():
         mt5.shutdown()
         return None
-    dados = mt5.copy_rates_from_pos(simbolo, timeframe, diaAtual, n)
+    dados = mt5.copy_rates_from_pos(simbolo, timeframe, delayCandles, n)
     if dados is None:
         return None
     dados = pd.DataFrame(dados)
