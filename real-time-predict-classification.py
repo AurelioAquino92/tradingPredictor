5c6a6a536c16a12effeb8cc5e607b53e49f85679
Aurelio Aquino <aurelio.obmep@gmail.com>
2023-08-29 11:06:02 -0400

    using more time information

--- real-time-predict-classification.py
+++ real-time-predict-classification.py
@@ -3,47 +3,71 @@ from obterDados import obterSimbolo
 from time import sleep
 from firebase_admin import credentials, firestore, initialize_app
 
-classificador = load(open('models/modeloClassificadorKNN.pickle', 'rb'))
 initialize_app(credentials.Certificate('key.json'))
 db = firestore.client()
 predictionsCollection = db.collection('predictions')
 
-count = 0
-actions = ['Nada', 'Compra', 'Venda']
+modelos = [
+    "ExtraTrees10000",
+    # "KNN10000",
+    "XGBoost10000"
+]
+
+classificadores = []
+for modelo in modelos:
+    classificadores.append(
+        load(open(f'models/modeloClassificador{modelo}.pickle', 'rb'))
+    )
+
+actionNames = ['Nada', 'Compra', 'Venda']
 
 lastPred = -1
 lastPrice = 0
 lastTimestamp = None
 
-# for i in range(110):
+# for i in range(40, -1, -1):
 while True:
-    dados = obterSimbolo('WDO$N', n=1000, delayCandles=0)
-    hist = dados.copy().drop(columns=['spread'])
-    hist['hour'] = hist.index.hour
-    histNP = hist.to_numpy()
-    vmax = histNP[:, :4].max()
-    vmin = histNP[:, :4].min()
-    histNP[:, :4] = (histNP[:, :4] - vmin) / (vmax - vmin)
-    vmax = histNP[:, 4].max()
-    vmin = histNP[:, 4].min()
-    histNP[:, 4] = (histNP[:, 4] - vmin) / (vmax - vmin)
-    vmax = histNP[:, 5].max()
-    vmin = histNP[:, 5].min()
-    histNP[:, 5] = (histNP[:, 5] - vmin) / (vmax - vmin)
-    histNP[:, 6] /= 24
-    previsao = int(classificador.predict([histNP.flatten()])[0])
-    
-    timestamp = hist.index[-1]
+    try:
+        dados = obterSimbolo('WDO$N', n=1000, delayCandles=0)
+        # dados = obterSimbolo('WDO$N', n=1000, delayCandles=i)
+        hist = dados.copy().drop(columns=['spread'])
+        hist['minute'] = hist.index.minute
+        hist['hour'] = hist.index.hour
+        hist['day_of_week'] = hist.index.day_of_week
+        hist['day'] = hist.index.day
+        histNP = hist.to_numpy()
+        vmax = histNP[:, :4].max()
+        vmin = histNP[:, :4].min()
+        histNP[:, :4] = (histNP[:, :4] - vmin) / (vmax - vmin)
+        vmax = histNP[:, 4].max()
+        vmin = histNP[:, 4].min()
+        histNP[:, 4] = (histNP[:, 4] - vmin) / (vmax - vmin)
+        vmax = histNP[:, 5].max()
+        vmin = histNP[:, 5].min()
+        histNP[:, 5] = (histNP[:, 5] - vmin) / (vmax - vmin)
+        histNP[:, 6] /= 60
+        histNP[:, 7] /= 24
+        histNP[:, 8] /= 4
+        histNP[:, 9] /= 31
 
-    if lastTimestamp != hist.index[-1] or lastPred != previsao:
-        print('nova previsão:', timestamp, actions[previsao])
-    if lastPrice != hist['close'][-1]:
-        predictionsCollection.document(str(timestamp)).set({
-            'previsao': actions[previsao],
-            'price': hist['close'][-1]
-        })
+        previsoes = {}
+        
+        for classificador in classificadores:
+            previsao = actionNames[int(classificador.predict([histNP.flatten()])[0])]
+            previsoes[modelos[classificadores.index(classificador)]] = previsao
+        
+        timestamp = hist.index[-1]
+        if lastTimestamp != hist.index[-1] or lastPred != previsao:
+            print('novas previsões:', timestamp, previsoes)
+        if lastPrice != hist['close'][-1]:
+            predictionsCollection.document(str(timestamp)).set({
+                'previsoes': previsoes,
+                'price': hist['close'][-1]
+            })
 
-    lastPred = previsao
-    lastPrice = hist['close'][-1]
-    lastTimestamp = hist.index[-1]
-    sleep(2)
+        lastPred = previsao
+        lastPrice = hist['close'][-1]
+        lastTimestamp = hist.index[-1]
+    except Exception as e:
+        print('Erro: ', e)
+    sleep(5)
