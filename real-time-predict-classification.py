30a276f1ae8c44451aa50c3059af91029dacb38c
Aurelio Aquino <aurelio.obmep@gmail.com>
2023-09-04 12:30:10 -0400

    salvando em arquivo o ultimo candle com previsoes

--- real-time-predict-classification.py
+++ real-time-predict-classification.py
@@ -4,6 +4,7 @@ from time import sleep
 from firebase_admin import credentials, firestore, initialize_app
 from keras.models import load_model
 import numpy as np
+from datetime import datetime
 
 initialize_app(credentials.Certificate('key.json'))
 db = firestore.client()
@@ -11,7 +12,6 @@ predictionsCollection = db.collection('predictions')
 
 modelos = [
     "ExtraTrees10000",
-    # "KNN10000",
     "XGBoost10000"
 ]
 
@@ -29,11 +29,10 @@ lastPred = -1
 lastPrice = 0
 lastTimestamp = None
 
-# for i in range(108*10, -1, -1):
-while True:
+def setPredictions(delay=0):
+    global lastPred, lastPrice, lastTimestamp
     try:
-        dados = obterSimbolo('WDO$N', n=1000, delayCandles=0)
-        # dados = obterSimbolo('WDO$N', n=1000, delayCandles=i)
+        dados = obterSimbolo('WDO$N', n=1000, delayCandles=delay)
         hist = dados.copy().drop(columns=['spread'])
         hist['minute'] = hist.index.minute
         hist['hour'] = hist.index.hour
@@ -64,6 +63,9 @@ while True:
         timestamp = hist.index[-1]
         if lastTimestamp != hist.index[-1] or lastPred != previsao:
             print('novas previsões:', timestamp, previsoes)
+            file = open('lastTimeStamp.txt', 'w')
+            file.write(str(hist.index[-1]))
+            file.close()
         if lastPrice != hist['close'][-1]:
             predictionsCollection.document(str(timestamp)).set({
                 'previsoes': previsoes,
@@ -75,4 +77,23 @@ while True:
         lastTimestamp = hist.index[-1]
     except Exception as e:
         print('Erro: ', e)
+
+updateFrom = 0
+try:
+    file = open('lastTimeStamp.txt', 'r')
+    lastTimestamp = datetime.strptime(file.read(), '%Y-%m-%d %H:%M:%S')
+    while True:
+        dados = obterSimbolo(delayCandles=updateFrom)
+        if dados.index[-1] >= lastTimestamp:
+            updateFrom += 1
+        else:
+            break
+except Exception as e:
+    print(e)
+
+for i in range(updateFrom, -1, -1):
+    setPredictions(i)
+
+while True:
+    setPredictions(i)
     sleep(5)
