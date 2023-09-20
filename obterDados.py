c417df4e6c7aa16d9eeb6b15fa0c8adb29b4879a
Aurelio Aquino <aurelio.obmep@gmail.com>
2023-09-20 11:39:38 -0400

    obtendo historico com grafico D1

--- obterDados.py
+++ obterDados.py
@@ -1,7 +1,8 @@
 import MetaTrader5 as mt5
 import pandas as pd
+from datetime import datetime
 
-def obterSimbolo(simbolo='WDO$N', timeframe=mt5.TIMEFRAME_M5, delayCandles=0, n=99999):
+def obterSimboloPosicao(simbolo='WDO$N', timeframe=mt5.TIMEFRAME_M5, delayCandles=0, n=99999):
     if not mt5.initialize():
         mt5.shutdown()
         return None
@@ -11,4 +12,16 @@ def obterSimbolo(simbolo='WDO$N', timeframe=mt5.TIMEFRAME_M5, delayCandles=0, n=
     dados = pd.DataFrame(dados)
     dados['time'] = pd.to_datetime(dados['time'], unit='s')
     dados.set_index('time', inplace=True)
-    return dados
+    return dados.drop(columns=['spread'])
+
+def obterSimboloData(dia, simbolo='WDO$N', timeframe=mt5.TIMEFRAME_D1, n=200):
+    if not mt5.initialize():
+        mt5.shutdown()
+        return None
+    dados = mt5.copy_rates_from(simbolo, timeframe, dia, n)
+    if dados is None:
+        return None
+    dados = pd.DataFrame(dados)
+    dados['time'] = pd.to_datetime(dados['time'], unit='s')
+    dados.set_index('time', inplace=True)
+    return dados.drop(columns=['spread'])
